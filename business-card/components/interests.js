676abf02e00812e45fccddcefdb3cc07312910e6
Abdul Rehman <beingabdulr@gmail.com>
2022-10-10 07:41:35 -0700

    Add Business Card Files

---
+++ business-card/components/interests.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Interests () {
+    return (
+        <div className="interests">
+            <h2 className="interests-title">Interests</h2>
+            <p className="interests-desc">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    );
+}
